87a93edb27736e697ce07ff51cc4f0572fcbfeaa
kayanev <kayane_v_santos@estudante.sesisenai.org.br>
2026-09-14 19:30:52 +0000

    aaaaaaaa

---
+++ main.py
@@ -0,0 +1,25 @@
+"""Atributos: id_usuario, nome, email, senha, tipo_permissao.
+Métodos: autenticar(), alterar_senha(), obter_dados_perfil().
+"""
+
+class Usuario:
+    def __init__(self, id_usuario, nome, email, senha, tipo_permissao):
+        self.id_usuario = "01"
+        self.nome = "admin"
+        self.email = "admin@gmail.com"
+        self.senha = "admin"
+        self.tipo_permissao = tipo_permissao
+
+    def menu(self):
+        inserir_nome = input("Insira o seu nome cadastrado aqui!")
+        
+
+    def autenticar():
+        
+
+    def alterar_senha():
+        pass
+
+    def obter_dados_perfil():
+        pass
+
