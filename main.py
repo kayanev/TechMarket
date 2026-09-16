ba75f17f1828d14b34b699ae80c86d6c504bd28d
kayanev <kayane_v_santos@estudante.sesisenai.org.br>
2026-09-16 14:22:50 -0300

    Refactor main.py to integrate Flask app

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
+from flask import Flask, render_template, request
+from classeproduto import * 
 from usuario import *
-from classeproduto import *
 from carrinho import *
+
+app = Flask(__name__)
+
+usuario_admin_padrao = Usuario(1, "admin", "admin@techmarket.com", "admin", "administrador")
+
+lista_produtos_memoria = [ Produto(3,"Queijo","Laticínios",22.0),
+    Produto(6,"Café Torrado e Moído",24.0),
+    Produto(2, "Laranja-pêra",6.0)
+    ]
+
+carrinho_atual = CarrinhoCompras()
+
+@app.route("/loja", methods=["GET"])
+def exibir_loja():
+    return render_template("loja.html", lista_produtos=lista_produtos_memoria)
+
+@app.route("/adicionar\_carrinho", methods=["POST"]) 
+def adicionar_ao_carrinho():
+    id_prod = request.form.get("id_produto,")
+    qtd_str = request.form.get("quantidade", "1")
+    msg = ""
+    try:
+        id_num = int(id_prod)
+        qtd = int(qtd_str)
+
+        produto_encontrado = None 
+        
+        for p in lista_produtos_memoria: 
+            if p.id_produto == id_num: produto_encontrado = p 
+            break
+
+        if produto_encontrado and produto_encontrado.quantidade_estoque >= qtd:
+            carrinho_atual.adicionar_item(produto_encontrado, qtd)
+            msg = f"'{produto_encontrado.nome}' adicionado ao carrinho!" 
+        
+        else: msg = "Erro: Quantidade solicitada indisponível no estoque!"
+
+    except ValueError:
+        msg = "Erro: Dados inválidos."
+
+    return render_template("loja.html", lista_produtos=lista_produtos_memoria, mensagem=msg)
 
 def main():
     usuario_admin_padrao = Usuario("01", "admin", "admin@gmail.com","admin", "Administrador")
@@ -17,5 +59,6 @@ def main():
     usuario_admin_padrao.alterar_senha("baleia")
     print(usuario_admin_padrao.obter_dados_perfil())
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__": 
+    app.run(debug=True)
+
